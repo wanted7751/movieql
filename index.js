8e441fef31d86c6db7b355ae7dfdfe8e91be0135
parkminho <minho4377@daum.net>
2019-01-13 08:00:33 +0900

    #4 Creating the first Query and Resolver

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
 
 const server = new GraphQLServer({
-
+    typeDefs:"./graphql/schema.graphql"   ,
+    //typeDefs는 모든 타입들에 대한 정의이다.
+    resolvers:resolvers  
 })
 
 server.start(() => console.log("Graphql Server Running"));
